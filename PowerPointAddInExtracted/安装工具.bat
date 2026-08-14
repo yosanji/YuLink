21ec1923d82849db18ba7154b79b5227fb11608c
yosanji <yosanji@users.noreply.github.com>
2026-08-14 13:27:27 +0800

    refactor: remove redundant legacy 安装工具.bat in subfolder and unify on root 一键安装.bat

--- PowerPointAddInExtracted/安装工具.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-title YuLink - 一键安装工具
-
-if exist "%~dp0..\一键安装.bat" (
-    call "%~dp0..\一键安装.bat"
-) else (
-    powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0..\build_and_deploy.ps1"
-)
